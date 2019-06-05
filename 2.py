fb50d344c9c5eb1249e007b3769475b309fa01b5
h-jia <i4761223>
2019-06-05 23:23:35 +1000

    unpack to binary array 01001001

--- 2.py
+++ 2.py
@@ -2,15 +2,10 @@
 
 import numpy as np
 # import urandom
-
 from os import urandom
 
 a = np.frombuffer(urandom(5), dtype=np.uint8)
 b = a.tobytes()
-# print(type(a))
-print(a)
-# print(type(b))
-print(b)
 c = np.frombuffer(b, dtype=a.dtype)
-# print(b.decode("utf-8"))
+d = np.unpackbits(c, axias=0)
 print(c)
